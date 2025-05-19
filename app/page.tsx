671c43a3628869044d5ca26fe53b071684ed3d88
Travis <Travisang40@gmail.com>
2025-05-19 17:15:50 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ import { Particles } from "@/components/magicui/particles";
 export default function Home() {
   return (
     <div className="px-5 flex flex-col gap-10 ">
-      <div className="fixed top-0 left-0 z-[-1]">
-      <Particles className="h-screen w-screen" color="#c452eb" size={1}/>
-    </div>
+      <div className="fixed top-0 left-0 z-[-1] lg:block hidden">
+        <Particles className="h-screen w-screen" color="#c452eb" size={1}/>
+      </div>
       <Hero />
       <BentoGrid />
       <Pricing />
